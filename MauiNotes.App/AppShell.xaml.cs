29b7500d7596a4d0e54831b9cf6600ddd5611220
Kris <kris_s2@outlook.com>
2025-11-28 22:46:25 -0800

    Add project files.

---
+++ MauiNotes.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiNotes.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MauiNotes.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiNotes.App
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
