1e079e449ae0f9618537140cb7ef602194e99bf6
Kris <kris_s2@outlook.com>
2025-11-30 12:15:30 -0800

    kata: adding clicked eventhandler for the button.

--- MauiNotes.App/Pages/CounterPage.xaml.cs
+++ MauiNotes.App/Pages/CounterPage.xaml.cs
@@ -2,8 +2,21 @@ namespace MauiNotes.App.Pages;
 
 public partial class CounterPage : ContentPage
 {
+	private int _initCount = 0;
+
 	public CounterPage()
 	{
 		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e) 
+	{
+		_initCount++;
+
+		CounterLabel.Text = _initCount == 1
+			? "Clicked 1 time."
+			: $"Clicked {_initCount} times.";
+
+		SemanticScreenReader.Announce(CounterLabel.Text);
 	}
 }
