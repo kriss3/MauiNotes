711e4e37992c71e8d075bf8ea5cd20e623be27fc
Kris <kris_s2@outlook.com>
2025-12-03 23:58:11 -0800

    kata: extracting interface

--- MauiNotes.Api/Services/WeatherService.cs
+++ MauiNotes.Api/Services/WeatherService.cs
@@ -1,21 +1,27 @@
 ﻿using MauiNotes.Api.Models;
 
-namespace MauiNotes.Api.Services;
-
-public class WeatherService
+namespace MauiNotes.Api.Services
 {
-	private static readonly string[] Summaries =
-	[
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	];
+    public interface IWeatherService
+    {
+        IEnumerable<WeatherForecast> GetRandomWeatherForecast();
+    }
 
-	public static IEnumerable<WeatherForecast> GetRandomWeatherForecast()
-	{
-		return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
-		{
-			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-			TemperatureC = Random.Shared.Next(-20, 55),
-			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-		})];
-	}
+    public class WeatherService : IWeatherService
+    {
+        private static readonly string[] Summaries =
+        [
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        ];
+
+        public IEnumerable<WeatherForecast> GetRandomWeatherForecast()
+        {
+            return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })];
+        }
+    }
 }
