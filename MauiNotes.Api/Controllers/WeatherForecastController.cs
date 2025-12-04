f9396d55bff447079d82c0a35141d3c39d410bf3
Kris <kris_s2@outlook.com>
2025-12-03 23:56:03 -0800

    kata moving controller method to the service.

--- MauiNotes.Api/Controllers/WeatherForecastController.cs
+++ MauiNotes.Api/Controllers/WeatherForecastController.cs
@@ -7,10 +7,7 @@ namespace MauiNotes.Api.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries =
-    [
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    ];
+    
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
@@ -18,14 +15,5 @@ public class WeatherForecastController : ControllerBase
         return GetRandomWeatherForecast();
     }
 
-    private static IEnumerable<WeatherForecast> GetRandomWeatherForecast()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
-    }
+    
 }
--- MauiNotes.Api/Services/WeatherService.cs
+++ MauiNotes.Api/Services/WeatherService.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiNotes.Api.Services;
+﻿using MauiNotes.Api.Models;
+
+namespace MauiNotes.Api.Services;
 
 public class WeatherService
 {
@@ -7,4 +9,13 @@ public class WeatherService
 		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 	];
 
+	public static IEnumerable<WeatherForecast> GetRandomWeatherForecast()
+	{
+		return [.. Enumerable.Range(1, 5).Select(index => new WeatherForecast
+		{
+			Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+			TemperatureC = Random.Shared.Next(-20, 55),
+			Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+		})];
+	}
 }
