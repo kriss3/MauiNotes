73fe5a9eeadebe0d3909121b5a3a24d02c00caa8
Kris <kris_s2@outlook.com>
2025-12-04 23:17:11 -0800

    kata: beginning on work on brand new controller.

---
+++ MauiNotes.Api/Controllers/NotesController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MauiNotes.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class NotesController : ControllerBase
+{
+}
