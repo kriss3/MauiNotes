2fa0d99e7f348aa07fa1c5d350e590cf8aa6d0c9
Kris <kris_s2@outlook.com>
2025-12-04 23:24:02 -0800

    kata: adding a scaffold for inMemory Notes repository.

---
+++ MauiNotes.Api/Repositories/InMemoryNoteRepository.cs
@@ -0,0 +1,32 @@
+﻿using MauiNotes.Api.Models;
+
+namespace MauiNotes.Api.Repositories;
+
+public class InMemoryNoteRepository : INoteRepository
+{
+    public Task<Note> AddAsync(Note note)
+    {
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Note>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Note?> GetByIdAsync(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Note?> UpdateAsync(Note note)
+    {
+        throw new NotImplementedException();
+    }
+}
