b596b8375a0c0b1922e03ceafcb0450dca754984
Kris <kris_s2@outlook.com>
2025-12-04 23:19:20 -0800

    kata: adding a new model

---
+++ MauiNotes.Api/Models/Note.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiNotes.Api.Models;
+
+public sealed class Note
+{
+	public int Id { get; set; }
+	public string Title { get; set; } = string.Empty;
+	public string Content { get; set; } = string.Empty;
+}
