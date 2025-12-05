5bd07281b5f9980e7593bd1ba1c31983b4a97f5f
Kris <kris_s2@outlook.com>
2025-12-04 23:21:24 -0800

    kata: adding Dtos and NoteDto

---
+++ MauiNotes.Api/Dtos/NoteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MauiNotes.Api.Dtos;
+
+public sealed class NoteDto
+{
+	public int Id { get; init; }
+	public string Title { get; init; } = string.Empty;
+	public string Content { get; init; } = string.Empty;
+}
