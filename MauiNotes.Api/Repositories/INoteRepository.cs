838276298fe93264573b2a7c19cfc2ac151b9363
Kris <kris_s2@outlook.com>
2025-12-04 23:22:50 -0800

    kata: adding basic Notes repository

---
+++ MauiNotes.Api/Repositories/INoteRepository.cs
@@ -0,0 +1,12 @@
+﻿using MauiNotes.Api.Models;
+
+namespace MauiNotes.Api.Repositories;
+
+public interface INoteRepository
+{
+	Task<List<Note>> GetAllAsync();
+	Task<Note?> GetByIdAsync(int id);
+	Task<Note> AddAsync(Note note);
+	Task<Note?> UpdateAsync(Note note);
+	Task<bool> DeleteAsync(int id);
+}
